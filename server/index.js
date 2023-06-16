1e0249223cb443ace4bb96b9eb50e1c458716e30
PIYUSH MALI <piyushmali702@gmail.com>
2023-06-16 14:58:02 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -8,19 +8,19 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = '...'; // Replace '...' with the actual Merkle root representing the nice list
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const { name, proof } = req.body;
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
-    res.send("You got a toy robot!");
-  }
-  else {
-    res.send("You are not on the list :(");
+  // TODO: prove that a name is in the list
+  const isInTheList = verifyProof(name, proof, MERKLE_ROOT);
+
+  if (isInTheList) {
+    res.send('You got a toy robot!');
+  } else {
+    res.send('You are not on the list :(');
   }
 });
 
